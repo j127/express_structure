10194ce186954c31978266482ea62ea56945b8bc
J. Cohen <hostelmarketing@gmail.com>
2020-04-24 00:16:35 -0700

    Fix error

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const ctrl = require("../controllers/apiController");
 
 const router = express.Router();
 
-// The runs the code stored in `controllers/pageController.js`
+// The runs the code stored in `controllers/apiController.js`
 router.get("/", ctrl.list);
 router.get("/:id", ctrl.detail);
 
